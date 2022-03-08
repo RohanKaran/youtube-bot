8d7d6c26d4c90b1674936d1236bd0c10275ac878
Rohan Karan <rohankaran001@gmail.com>
2022-03-09 03:41:52 +0530

    .

---
+++ main.py
@@ -0,0 +1,11 @@
+import datetime
+from selenium.webdriver.common.by import By
+from youtube.youtube import YouTube
+from google_client.gspwrite import write
+
+with YouTube(teardown=True) as bot:
+    bot.land_first_page()
+    rand = bot.find_elements(By.ID, "details")
+    for i in range(10):
+        write([[str(datetime.datetime.now()), rand[i].find_element(By.ID, "video-title").text,
+              rand[i].find_element(By.ID, "channel-name").text]])

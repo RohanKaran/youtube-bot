03ef32b11af5bf8bc74c1ac74abf478b1a8b842c
Rohan Karan <rohankaran001@gmail.com>
2022-03-09 09:24:44 +0530

    .

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ with YouTube(teardown=True) as bot:
     bot.land_first_page()
     rand = bot.find_elements(By.ID, "details")
     n = 10
-    while n > 0:
-        print( rand[i].find_element(By.ID, "video-title").text,
-                rand[i].find_element(By.ID, "channel-name").text)
+    for i in range(n):
         write([[str(datetime.datetime.now()), rand[i].find_element(By.ID, "video-title").text,
                 rand[i].find_element(By.ID, "channel-name").text]])
-        n -= 1
